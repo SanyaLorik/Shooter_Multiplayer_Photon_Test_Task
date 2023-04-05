7d1c4e6d813e98b482e23f3ba3a3a788e13a0e9b
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-04-06 00:15:22 +0300

    add: waiting panel

---
+++ Assets/Project/Scripts/Ui/UiWaitingPanel.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.Collections;
+using UnityEngine;
+
+namespace Shooter
+{
+    public class UiWaitingPanel : MonoBehaviourPunCallbacks
+    {
+        [SerializeField] private GameObject _panel;
+
+        private const int _minPlayer = 2;
+
+        private void Start()
+        {
+            if (PhotonNetwork.CountOfPlayers >= _minPlayer)
+                _panel.SetActive(false);
+        }
+
+        public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+        {
+            _panel.SetActive(false);
+        }
+    }
+}
