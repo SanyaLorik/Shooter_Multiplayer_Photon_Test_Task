ac3aec543981065e4b86ad97258a8b0b9e4e795a
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-04-05 20:46:48 +0300

    add: launcj projectile

---
+++ Assets/Project/Scripts/Battle/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Shooter
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField][Range(0f, 10f)] private float _force;
+        [SerializeField][Min(0)] private float _damage;
+
+        public void Launch(Vector2 direction)
+        {
+            _rigidbody.AddForce(direction * _force, ForceMode2D.Impulse);
+        }
+    }
+}
--- Assets/Project/Scripts/Battle/Weapon.cs
+++ Assets/Project/Scripts/Battle/Weapon.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 
 namespace Shooter
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] private Projectile _projectile;
+
         private IShootingObservable _shootingObservable;
 
         private void Awake()
@@ -24,6 +27,12 @@ namespace Shooter
 
         private void OnShoot()
         {
+            var projectile = PhotonNetwork
+                .Instantiate(_projectile.name, transform.position + transform.up, Quaternion.identity)
+                .GetComponent<Projectile>();
+
+            projectile.Launch(transform.up);
+
             Debug.Log("Shoot!");
         }
     }
