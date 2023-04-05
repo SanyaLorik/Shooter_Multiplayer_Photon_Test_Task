1a99a20cb116c13a4fae017e81bbcfe7973d1cb5
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-04-05 23:48:30 +0300

    rollback

---
+++ Assets/Project/Scripts/Entity/PlayerColorChanger.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace Shooter
+{
+    public class PlayerColorChanger : MonoBehaviour
+    {
+        [SerializeField] private PhotonView _view;
+        [SerializeField] private SpriteRenderer[] _spriteRenderers;
+        [SerializeField] private Color[] _colors;
+        [SerializeField] private RpcTarget _rpcTarget;
+
+        private void Start()
+        {
+            var color = _colors[PhotonNetwork.CountOfPlayers - 1];
+            var v =  PhotonView.Get(this);
+            Change(color.r, color.g, color.b, color.a);
+            //v.RPC("Change", _rpcTarget, color.r, color.g, color.b, color.a);
+            //v.RPC("Change", RpcTarget.AllBuffered, 4f, 4f, 4f, 4f);
+        }
+
+        [PunRPC]
+        private void Change(float r, float g, float b, float a)
+        {
+            foreach (var spriteRenderer in _spriteRenderers)
+                spriteRenderer.color = new Color(r, g, b, a);
+        }
+       
+        /*
+        private void Change(Color color)
+        {
+            foreach (var spriteRenderer in _spriteRenderers)
+                spriteRenderer.color = color;
+        }
+        */
+    }
+}
