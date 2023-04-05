c6c258b3024de58cce82b04fd8b07a6d642cf03f
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-04-05 21:33:01 +0300

    add: healthbar

---
+++ Assets/Project/Scripts/Additional/Follower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Shooter
+{
+    public class Follower : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private Vector3 _offset;
+
+        private void Update()
+        {
+            transform.position = _target.position + _offset;
+        }
+    }
+}
