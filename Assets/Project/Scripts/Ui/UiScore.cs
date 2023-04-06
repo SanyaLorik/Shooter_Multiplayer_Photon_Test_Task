708e5fec614b9f9389effc3b598f857ac7dd50be
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-04-06 03:30:49 +0300

    fix: score

--- Assets/Project/Scripts/Ui/UiScore.cs
+++ Assets/Project/Scripts/Ui/UiScore.cs
@@ -1,13 +1,21 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Shooter
 {
     public class UiScore : MonoBehaviour
     {
+        [SerializeField] private PhotonView _view;
         [SerializeField] private Text _score;
 
         public void SetScore(int score)
+        {
+            _view.RPC(nameof(SetScoreRPC), RpcTarget.AllBuffered, score);
+        }
+
+        [PunRPC]
+        private void SetScoreRPC(int score)
         {
             _score.text = score.ToString();
         }
